490f25628f1d52b8788475f12dc7b6fac6403146
peter ramsis <peterzizo100@gmail.com>
2020-03-14 12:56:26 +0200

    navbar

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,19 @@ var browser = require("browser-sync");
 var rename = require("gulp-rename");
 var autoprefixer = require("gulp-autoprefixer");
 var sourcemaps = require("gulp-sourcemaps");
-const image = require('gulp-image');
 var sassImport = require('gulp-sass-import')
 
 gulp.task("sass", function() {
     return gulp.src(["src/scss/**/*.scss"])
         .pipe(sassImport({
             filename: '_file',
-
         }))
         .pipe(sass({ outputStyle: "compressed" }))
         .pipe(autoprefixer('last 2 versions'))
         .pipe(concat("main.min.css"))
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("dist/css/"))
-        .pipe(browser.stream());;
+        .pipe(browser.stream());
 });
 
 gulp.task("bootstrap", async function() {
@@ -35,7 +33,7 @@ gulp.task("bootstrap", async function() {
 
 gulp.task("watch", function() {
 
-    gulp.watch(["src/scss/**/*.scss", "src/scss/*.scss", ], gulp.parallel('sass'));
+    gulp.watch(["src/scss/*.scss", ], gulp.parallel('sass'));
     browser.init({
         server: {
             baseDir: "./"
